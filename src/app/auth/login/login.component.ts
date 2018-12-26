6b12ec840f41a13a5e83262fbc7dba6fa8e5a2dd
LiuBo <ok_liubo@sina.com>
2018-12-26 16:00:42 +0800

    fix result type

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -15,25 +15,8 @@ export class LoginComponent {
   }
 
   onLogin(form: NgForm) {
-    // const email = form.value.email;
-    // const password = form.value.password;
-    // this.authService.login(email, password);
-    const provider = new firebase.auth.GithubAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
+    const email = form.value.email;
+    const password = form.value.password;
+    this.authService.login(email, password);
   }
 }
