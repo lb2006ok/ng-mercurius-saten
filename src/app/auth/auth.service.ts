44eb1110a45d9bea54511ee160dd7141e770e9f2
LiuBo <ok_liubo@sina.com>
2018-12-26 15:49:05 +0800

    update node version

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,12 +5,17 @@ export class AuthService {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
         error => console.log(error)
-      )
+      );
   }
+
   login(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .catch(
         error => console.log(error)
-      )
+      );
+  }
+
+  getIdToken() {
+    return firebase.auth().currentUser.getIdToken();
   }
 }
